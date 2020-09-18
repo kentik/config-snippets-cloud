96d0252dc9415a6e04a0e637f61711e75f429f16
Dawid Trzebiatowski <boscard@serveraptor.com>
2020-09-18 21:52:43 +0200

    Adding sock-shop back

---
+++ cloud_AWS/terraform/module/examples/sock-shop-eks/variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "boutique-k8s-kentik"
+}
+
+variable "worker_node_type" {
+  type    = string
+  default = "t3a.medium"
+}
+
+variable "worker_node_count" {
+  type    = number
+  default = 2
+}
+
+variable "k8s_version" {
+  type    = string
+  default = "1.14"
+}
