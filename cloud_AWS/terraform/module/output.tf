567246028d711678ef5f9554c2a7896589afb554
Dawid Trzebiatowski <boscard@serveraptor.com>
2020-09-28 16:04:03 +0200

    Updating output from module

--- cloud_AWS/terraform/module/output.tf
+++ cloud_AWS/terraform/module/output.tf
@@ -5,6 +5,6 @@ output "iam_role_arn" {
 output "bucket_name_list" {
   value = [
     for bucketobject in aws_s3_bucket.vpc_logs :
-    bucketobject.bucket
+    (var.s3_flowlogs_path == "" ? bucketobject.bucket : "${bucketobject.bucket}/${var.s3_flowlogs_path}")
   ]
 }
