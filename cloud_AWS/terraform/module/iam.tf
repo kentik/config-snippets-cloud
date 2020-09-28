c529a8662a367a716a249688bfb9e253187d692b
Dawid Trzebiatowski <boscard@serveraptor.com>
2020-09-28 14:25:21 +0200

    Using one template only

--- cloud_AWS/terraform/module/iam.tf
+++ cloud_AWS/terraform/module/iam.tf
@@ -19,14 +19,20 @@ resource "aws_iam_policy" "kentik_s3_ro_access" {
   name        = "${var.iam_role_prefix}S3ROAccess"
   description = "Defines read-only accesses for Kentik platform to S3 resources"
   path        = "/"
-  policy      = templatefile("${path.module}/templates/kentikS3RO.json.tmpl",{buckets_list = [for bucketobject in aws_s3_bucket.vpc_logs : bucketobject.arn]})
+  policy      = templatefile(
+                  "${path.module}/templates/kentikS3Policy.json.tmpl",
+                  {actions_list = ["s3:Get*","s3:List*","s3:HeadBucket"],
+                  buckets_list = [for bucketobject in aws_s3_bucket.vpc_logs : bucketobject.arn]})
 }
 
 resource "aws_iam_policy" "kentik_s3_rw_access" {
   name        = "${var.iam_role_prefix}S3RWAccess"
   description = "Defines read-write accesses for Kentik platform to S3 resources"
   path        = "/"
-  policy      = templatefile("${path.module}/templates/kentikS3RW.json.tmpl",{buckets_list = [for bucketobject in aws_s3_bucket.vpc_logs : bucketobject.arn]})
+  policy      = templatefile(
+                  "${path.module}/templates/kentikS3Policy.json.tmpl",
+                  {actions_list = ["s3:*"],
+                  buckets_list = [for bucketobject in aws_s3_bucket.vpc_logs : bucketobject.arn]})
 
 }
 
