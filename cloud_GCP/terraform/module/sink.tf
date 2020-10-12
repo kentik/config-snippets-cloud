a30026f15b92797eeb0ff9593a68a5ffb1ab8a0f
mradaszewski <maciej.radaszewski@gmail.com>
2020-10-12 09:53:23 +0200

    Fixed filter for gather logs from multiple subnets

--- cloud_GCP/terraform/module/sink.tf
+++ cloud_GCP/terraform/module/sink.tf
@@ -1,19 +1,17 @@
-// Create sink to gather logs from subnetwork. One sink per subnet
+	// Create sink to gather logs from subnetwork. One sink per subnet
 resource "google_logging_project_sink" "kentik_sink" {
-  for_each    = toset(var.subnets_names_list)
-  name        = "${var.sink_prefix}_${each.key}"
+  name        = "${var.sink_prefix}_sink"
 
   destination = "pubsub.googleapis.com/projects/kentik-291211/topics/${var.topic_prefix}_topic"
 
-  filter = "resource.type = gce_subnetwork AND resource.labels.subnetwork_name = ${each.key}"
+  filter  = "resource.type = gce_subnetwork AND ( resource.labels.subnetwork_name = ${join(" OR resource.labels.subnetwork_name = ", var.subnets_names_list)} )"
 
   unique_writer_identity = true
 }
 // Provides publisher role to each sink
 resource "google_project_iam_binding" "log_writer" {
-  for_each    = toset(var.subnets_names_list)
   role        = "roles/pubsub.publisher"
   members = [
-    google_logging_project_sink.kentik_sink[each.key].writer_identity,
+    google_logging_project_sink.kentik_sink.writer_identity,
   ]
 }
