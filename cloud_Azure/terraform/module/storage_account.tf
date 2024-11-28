dfc81f0ebeaca9c37a66197c9f383398a8d947ab
jksprattler <jksprattler@users.noreply.github.com>
2024-11-27 18:20:48 -0600

    test

--- cloud_Azure/terraform/module/storage_account.tf
+++ cloud_Azure/terraform/module/storage_account.tf
@@ -1,7 +1,7 @@
 # Prepare names that meet Azure Storage Account naming restrictions (only alphanum letters, max 24 length, Azure-wide unique)
 # Each output name is concatenation of Resource Group name and Subscription ID, adjusted to naming restrictions
 locals {
-  _names                          = [for nsg in local.flat_nsgs : "${nsg.name}${var.subscription_id}"]
+  _names                          = [for nsg in local.flat_nsgs : "${nsg.value.name}${var.subscription_id}"]
   _lowercase_names                = [for name in local._names : lower(name)]
   _alphanum_lowercase_names       = [for name in local._lowercase_names : join("", regexall("[[:alnum:]]+", name))]
   generated_storage_account_names = [for name in local._alphanum_lowercase_names : substr(name, 0, 24)]
@@ -16,11 +16,11 @@ locals {
 # StorageAccounts are mapped 1:1+:1 to nsg(s) and resource_group_names
 # Note that only one flow log can be associated with a storage account per region
 resource "azurerm_storage_account" "logs_storage_account" {
-  for_each = { for nsg in local.flat_nsgs : nsg.key => nsg.value }
+  for_each = local.nsg_to_storage_account_name
 
   # Generate storage account per nsg(s) in each rg
-  name                     = local.generated_storage_account_names
-  resource_group_name      = each.value.rg
+  name                     = each.value
+  resource_group_name      = local.flat_nsgs[each.key].rg
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
