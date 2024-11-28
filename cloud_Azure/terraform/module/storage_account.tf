bb4b9a82770ac157aacce21c6ce23d991320d1fc
jksprattler <jksprattler@users.noreply.github.com>
2024-11-27 18:00:42 -0600

    test

--- cloud_Azure/terraform/module/storage_account.tf
+++ cloud_Azure/terraform/module/storage_account.tf
@@ -19,7 +19,7 @@ resource "azurerm_storage_account" "logs_storage_account" {
   for_each = { for nsg in local.flat_nsgs : nsg.key => nsg.value }
 
   # Generate storage account per nsg(s) in each rg
-  name                     = local.nsg_to_storage_account_name
+  name                     = local.generated_storage_account_names
   resource_group_name      = each.value.rg
   location                 = var.location
   account_tier             = "Standard"
