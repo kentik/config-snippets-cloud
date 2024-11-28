6295474973f3a9f9904b295f07d052999076e808
jksprattler <jksprattler@users.noreply.github.com>
2024-11-27 18:44:38 -0600

    try index function

--- cloud_Azure/terraform/module/storage_account.tf
+++ cloud_Azure/terraform/module/storage_account.tf
@@ -19,7 +19,7 @@ resource "azurerm_storage_account" "logs_storage_account" {
   for_each = { for nsg in local.flat_nsgs : nsg.key => nsg.value }
 
   # Generate storage account per nsg(s) in each rg
-  name                     = local.generated_storage_account_names[each.key]
+  name                     = local.generated_storage_account_names[index(local.flat_nsgs[*].key, each.key)]
   resource_group_name      = each.value.rg
   location                 = var.location
   account_tier             = "Standard"
